7cc9240bb63ecadcd77038639cbed6fe7e990857
Guilherme Leoi <geeleonidas@pm.me>
2023-08-05 14:30:14 -0300

    Draw "DEBUG" on the screen

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import SDL
 import SDL.Image
+import SDL.Font
 
 import Data.Text (pack)
 import Control.Monad (unless)
@@ -12,20 +13,28 @@ import Currymon
 main :: IO ()
 main = do
   initializeAll
+  SDL.Font.initialize
   window <- createWindow (pack "Currymon") defaultWindow
   renderer <- createRenderer window (-1) defaultRenderer
-  appLoop renderer
+  appLoop window renderer
   destroyWindow window
+  SDL.Font.quit
 
-appLoop :: Renderer -> IO ()
-appLoop renderer = do
+appLoop :: Window -> Renderer -> IO ()
+appLoop window renderer = do
   events <- pollEvents
   let exiting = any eventIsExit events
 
   texture <- loadTexture renderer "./res/battle-concept1.png"
+  font <- SDL.Font.load "./res/font/public-pixel/PublicPixel.ttf" 16
+  surface <- SDL.Font.solid font (V4 255 0 0 0) (pack "DEBUG")
+  text <- createTextureFromSurface renderer surface
   clear renderer
   copy renderer texture Nothing (Just $ Rectangle (P $ V2 0 0) (V2 800 600))
+  copy renderer text Nothing (Just $ Rectangle (P $ V2 0 0) (V2 667 134))
   present renderer
   destroyTexture texture
+  SDL.Font.free font
+  freeSurface surface
   
-  unless exiting (appLoop renderer)
+  unless exiting (appLoop window renderer)

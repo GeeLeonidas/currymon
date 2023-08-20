7cc4c8ee850e6cd6b297f5ac28b6949da37628bb
Guilherme Leoi <geeleonidas@pm.me>
2023-08-20 00:49:24 -0300

    Add helper functions and procedures

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,14 @@ import Data.Text (pack)
 import Control.Monad (unless)
 
 import Currymon
+import Control.Concurrent (threadDelay)
 
 
 main :: IO ()
 main = do
   initializeAll
   SDL.Font.initialize
-  window <- createWindow (pack "Currymon") defaultWindow
+  window <- createWindow (pack "Currymon") gameWindowConfig
   renderer <- createRenderer window (-1) defaultRenderer
   appLoop window renderer
   destroyWindow window
@@ -25,16 +26,25 @@ appLoop window renderer = do
   events <- pollEvents
   let exiting = any eventIsExit events
 
-  texture <- loadTexture renderer "./res/battle-concept1.png"
-  font <- SDL.Font.load "./res/font/public-pixel/PublicPixel.ttf" 16
-  surface <- SDL.Font.solid font (V4 255 0 0 0) (pack "DEBUG")
-  text <- createTextureFromSurface renderer surface
-  clear renderer
-  copy renderer texture Nothing (Just $ Rectangle (P $ V2 0 0) (V2 800 600))
-  copy renderer text Nothing (Just $ Rectangle (P $ V2 0 0) (V2 667 134))
-  present renderer
-  destroyTexture texture
-  SDL.Font.free font
-  freeSurface surface
+  -- TODO: Move these 'load'/'create' procedures outside the loop
+  bg <- loadTexture renderer "./res/battle-concept1.png"
+  font <- SDL.Font.load "./res/font/public-pixel/PublicPixel.ttf" 8
+  tSur <- SDL.Font.solid font (V4 255 0 0 0) (pack "DEBUG")
+  text <- createTextureFromSurface renderer tSur
   
+  bgBounds <- textureBounds bg
+  textBounds <- textureBounds text
+
+  clear renderer
+  copy renderer bg Nothing $ Just $ Rectangle (P $ V2 0 0) (renderScale <$> bgBounds)
+  copy renderer text Nothing $ Just $ Rectangle (P $ V2 0 0) (renderScale <$> textBounds)
+  present renderer
+
+  -- TODO: Also move these 'free'/'destroy' procedures
+  SDL.Font.free font
+  freeSurface tSur
+  destroyTexture bg
+  destroyTexture text
+
+  threadDelay 30000
   unless exiting (appLoop window renderer)
--- src/Currymon.hs
+++ src/Currymon.hs
@@ -1,10 +1,41 @@
-module Currymon (eventIsExit) where
+module Currymon (
+    gameRes
+  , renderScale
+  , gameWindowConfig
+  , eventIsExit
+  , textureBounds
+  ) where
 
 import SDL
+import Foreign.C
+import Control.Monad.IO.Class (MonadIO)
 
+
+gameRes :: Integral a => V2 a
+gameRes = V2 160 144
+
+renderScale :: Integral a => a -> a
+renderScale = (* 4)
+
+gameWindowConfig :: WindowConfig
+gameWindowConfig = WindowConfig {
+    windowVisible         = True
+  , windowResizable       = False
+  , windowPosition        = Wherever
+  , windowMode            = Windowed
+  , windowInputGrabbed    = False
+  , windowInitialSize     = renderScale <$> gameRes
+  , windowHighDPI         = False
+  , windowGraphicsContext = NoGraphicsContext
+  , windowBorder          = True
+  }
 
 eventIsExit :: Event -> Bool
 eventIsExit event =
   case eventPayload event of
     WindowClosedEvent _ -> True
     _ -> False
+
+textureBounds :: MonadIO m => Texture -> m (V2 CInt)
+textureBounds t = bounds <$> queryTexture t
+  where bounds info = V2 (textureWidth info) (textureHeight info)

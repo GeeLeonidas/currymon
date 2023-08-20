3b8a6a0e0d1e54fbf43c1fab81594d859186f15c
Guilherme Leoi <geeleonidas@pm.me>
2023-08-20 02:50:13 -0300

    Add asset loading procedures

--- src/Currymon.hs
+++ src/Currymon.hs
@@ -2,13 +2,24 @@ module Currymon (
     gameRes
   , renderScale
   , gameWindowConfig
+  , spritePaths
+  , fontPaths
+  , loadSprites
+  , loadFonts
+  , destroySprites
+  , freeFonts
   , eventIsExit
   , textureBounds
   ) where
 
 import SDL
+import SDL.Image
+import SDL.Font
+
 import Foreign.C
 import Control.Monad.IO.Class (MonadIO)
+import Data.HashMap.Strict
+import System.FilePath
 
 
 gameRes :: Integral a => V2 a
@@ -30,11 +41,55 @@ gameWindowConfig = WindowConfig {
   , windowBorder          = True
   }
 
+spritePaths :: [FilePath]
+spritePaths = [
+    "./res/battle-concept1.png"
+  ]
+
+fontPaths :: [FilePath]
+fontPaths = [
+    "./res/font/public-pixel/PublicPixel.ttf"
+  ]
+
 eventIsExit :: Event -> Bool
 eventIsExit event =
   case eventPayload event of
     WindowClosedEvent _ -> True
     _ -> False
+
+loadSprites :: MonadIO m => Renderer -> [FilePath] -> m (HashMap String Texture)
+loadSprites _ []     = pure empty
+loadSprites r (x:ys) = union <$> (singleton key <$> loadTexture r x) <*> recur
+  where
+    key = takeBaseName x
+    recur = loadSprites r ys
+
+loadFonts :: MonadIO m => Renderer -> [FilePath] -> PointSize -> m (HashMap String Font)
+loadFonts _ [] _     = pure empty
+loadFonts r (x:ys) n = union <$> (singleton key <$> SDL.Font.load x n) <*> recur
+  where
+    key = takeBaseName x
+    recur = loadFonts r ys n
+
+destroySprites :: MonadIO m => HashMap String Texture -> m ()
+destroySprites hm
+  | hm == empty = pure ()
+  | otherwise   = do
+    destroyTexture texture
+    recur
+  where
+    (key, texture) = head $ toList hm
+    recur = destroySprites $ delete key hm
+
+freeFonts :: MonadIO m => HashMap String Font -> m ()
+freeFonts hm
+  | hm == empty = pure ()
+  | otherwise   = do
+    SDL.Font.free font
+    recur
+  where
+    (key, font) = head $ toList hm
+    recur = freeFonts $ delete key hm
 
 textureBounds :: MonadIO m => Texture -> m (V2 CInt)
 textureBounds t = bounds <$> queryTexture t
